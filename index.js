de9453170a11436ed1e1bff699957419bb892f7f
Matthew Wiram <matthew.wiram551@gmail.com>
2023-11-13 19:34:47 -0500

    sdf

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const {getFirestore} = require("firebase-admin/firestore");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
