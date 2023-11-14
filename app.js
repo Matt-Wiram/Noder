88ac5735232600432b2bda45a9e3468bf6cd4e4b
Matthew Wiram <matthew.wiram551@gmail.com>
2023-11-13 19:44:17 -0500

    asdf

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var logger = require('morgan');
 
 
 
-const { initializeApp, applicationDefault, cert } = require('./firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue, Filter } = require('./firebase-admin/firestore');
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
 const serviceAccount = require('./serviceAccount.json');
 
